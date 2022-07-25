a4e4c5a1ae6df52f683aee4eaf6d4c40874a5a7b
tjuantong <77320112+tjuantong@users.noreply.github.com>
2022-07-25 15:00:58 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world!</h1>
+        <h1>Hello there!</h1>
         <p>
           Sample React app for AWS CLI 
         </p>
